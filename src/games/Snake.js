99b44692684ac6e5d4a9e0139b257e1488dda796
Igor <bigbookreader@gmail.com>
2022-09-24 19:45:52 +0300

    Adding script in useEffect and fixed Snake bugs

--- src/games/Snake.js
+++ src/games/Snake.js
@@ -1,48 +1,43 @@
 import React, { useEffect } from "react";
+
+export default function Snake() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "snake-func.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Snake game</h2>
+      <canvas id="canvas" width={400} height={400} />
+    </div>
+  );
+}
+
+/*
+// This works, you only need to wrap the script snake-func.js in an export default function SG() {}
+
+import React, { useEffect } from "react";
+
 import SG from "./snake-func";
 
 export default function Snake() {
   useEffect(() => {
-    // const script = document.createElement("script");
-
-    // script.src = "../src/games/snake-func.js";
-    // script.async = true;
-
-    // document.body.appendChild(script);
-
-    //const canvas = document.getElementById("canvas2");
-
-    //if (canvas) {
     console.log("Privet");
     SG();
-    //}
-
-    // return () => {
-    //   document.body.removeChild(script);
-    // };
   }, []);
 
   return (
     <div id="canvas2">
       <h2>Snake game</h2>
-
       <canvas id="canvas" width={400} height={400}></canvas>
-
-      {/* <script src="./snake-func.js"> </script> */}
     </div>
   );
 }
-
-// import React from "react";
-// import Snake_func from "./snake-func";
-
-// export default function Snake() {
-//   return (
-//     <div>
-//       <h2>Snake</h2>
-//       <body>
-//         <script src="./snake-func"></script>
-//       </body>
-//     </div>
-//   );
-// }
+*/
