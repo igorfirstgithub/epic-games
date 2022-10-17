f37ca9856b2376fad3d7206b08d080457a02bd38
Igor <bigbookreader@gmail.com>
2022-10-17 12:40:39 +0300

    Tennis turned into classes

--- src/pages/Tennis.js
+++ src/pages/Tennis.js
@@ -1,63 +1,37 @@
 //--------- initial simple version -----------
-import React from "react";
+// import React from "react";
 
-export default function Tennis() {
-  return (
-    <div>
-      <h2>Tennis</h2>
-    </div>
-  );
-}
+// export default function Tennis() {
+//   return (
+//     <div>
+//       <h2>Tennis</h2>
+//     </div>
+//   );
+// }
 
-/* import React, { useEffect, useRef, useState } from "react";
-import { Tennis } from "../games/TennisTypes.js";
+//--------------------------------------------------------
+
+import React, { useEffect, useRef } from "react";
+import { Tennis } from "../games/TennisTypes";
 
 const TennisGame = () => {
   const ref = useRef(HTMLCanvasElement);
-  const [tennis, setTennis] = useState();
-  const [isGameRunning, setIsGameRunning] = useState(false);
 
   useEffect(() => {
     if (ref.current) {
       const context = ref.current.getContext("2d");
       if (context) {
-        console.log(tennis);
-        setTennis(new Tennis(context));
+        const tennis = new Tennis(context);
+        tennis.start();
       }
     }
   }, [ref]);
 
-  useEffect(() => {
-    if (tennis) {
-      tennis.start();
-      tennis.pause();
-    }
-  }, [tennis]);
-
-  function startNewGame() {
-    if (tennis.isGameOver) {
-      tennis.restart();
-      console.log(tennis.timeoutID);
-    } else if (tennis.pauseGame && !isGameRunning) {
-      console.log(tennis.timeoutID);
-      tennis.pause();
-      setIsGameRunning(true);
-    }
-  }
-
-  function pauseGame() {
-    if (isGameRunning && !tennis.isGameOver) tennis.pause();
-  }
-
   return (
     <div>
-      <h3>Tennis game</h3>
-      {<button onClick={startNewGame}>Start new game</button>}
-      {isGameRunning && <button onClick={pauseGame}>Pause</button>}
-
+      <h2>Tennis game</h2>
       <canvas ref={ref} width={400} height={400} />
     </div>
   );
 };
-
-export default TennisGame; */
+export default TennisGame;
